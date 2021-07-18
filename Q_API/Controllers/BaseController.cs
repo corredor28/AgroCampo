6125eb4ca959e739748c3826904eab8aff803b15
David Corredor <corredor28@gmail.com>
2021-07-17 21:29:04 -0500

    Add Persona controller

---
+++ Q_API/Controllers/BaseController.cs
@@ -0,0 +1,37 @@
+﻿using F_Data;
+using L_Business;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Q_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BaseController<T> : ControllerBase where T : class, new()
+    {
+        protected readonly AgroCampoContext _dbContext;
+
+        public BaseController(AgroCampoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> Get()
+        {
+            try
+            {
+                var bl = new BaseBL<T>(_dbContext);
+                var result = await bl.Get();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Q_API/Controllers/PersonaController.cs
@@ -0,0 +1,34 @@
+﻿using F_Data;
+using F_Data.EFEntities;
+using L_Business;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Q_API.Controllers
+{
+    public class PersonaController : BaseController<Persona>
+    {
+        public PersonaController(AgroCampoContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        [HttpGet("persona/{documento}")]
+        public async Task<IActionResult> GetBy(string documento)
+        {
+            try
+            {
+                var bl = new PersonaBL(_dbContext);
+                var result = await bl.GetBy(documento);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
