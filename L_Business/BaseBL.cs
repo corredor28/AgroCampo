06d98a7c49b8107db39ea2d0182e516c37443a09
David Corredor <corredor28@gmail.com>
2021-07-17 21:22:18 -0500

    Add Persona business class

---
+++ L_Business/BaseBL.cs
@@ -0,0 +1,28 @@
+﻿using F_Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_Business
+{
+    public class BaseBL<T> where T : class, new()
+    {
+        protected readonly AgroCampoContext _dbContext;
+
+        public BaseBL(AgroCampoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<IEnumerable<T>> Get()
+        {
+            var resultado = await _dbContext.Set<T>().AsNoTracking().ToListAsync();
+            return resultado;
+        }
+
+
+    }
+}
---
+++ L_Business/PersonaBL.cs
@@ -0,0 +1,25 @@
+﻿using F_Data;
+using F_Data.EFEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_Business
+{
+    public class PersonaBL : BaseBL<Persona>
+    {
+        public PersonaBL(AgroCampoContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public async Task<Persona> GetBy(string documento)
+        {
+            var result = await _dbContext.Persona.AsNoTracking().FirstOrDefaultAsync(m => m.Documento == documento);
+            return result;
+        }
+    }
+}
